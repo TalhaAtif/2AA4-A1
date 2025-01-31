15557b1902e8ce3bd445371d09aea066bf9a912e
_tvd <talha.atif.89@gmail.com>
2025-01-31 14:56:41 -0500

    Rewrote direction enum (close #15)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Direction.java
@@ -3,17 +3,11 @@ package ca.mcmaster.se2aa4.mazerunner;
 public enum Direction {
     NORTH, EAST, SOUTH, WEST;
 
-    public Direction next() {
-        Direction[] vals = Direction.values();
-        int i = this.ordinal();
-        int next = (i + 1) % vals.length;
-        return vals[next];
+    public Direction turnRight() {
+        return values()[(ordinal() + 1) % 4];
     }
 
-    public Direction prev() {
-        Direction[] vals = Direction.values();
-        int i = this.ordinal();
-        int prev = (i - 1 + vals.length) % vals.length;
-        return vals[prev];
+    public Direction turnLeft() {
+        return values()[(ordinal() + 3) % 4];
     }
 }
